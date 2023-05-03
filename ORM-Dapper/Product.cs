2c5ced92ae7070353575911934736120cdf95481
taylor Schuchter <taylorschuchter@gmail.com>
2023-05-03 15:59:03 -0400

    project complete

---
+++ ORM-Dapper/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int categoryId { get; set; }
+        public bool onSale { get; set; }
+        public int stockLevel { get; set; }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Setup
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+            #endregion
+            #region Products
+            var productRepo = new DapperProductRepository(conn);
+            //productRepo.CreateProduct("potato", 3.50, 10);
+            //Product updateProduct = new Product();
+            //updateProduct.price = 42.50;
+            //updateProduct.name = "Fancy Potato";
+            //updateProduct.onSale = false;
+            //updateProduct.stockLevel = 999;
+            //updateProduct.categoryId = 1;
+            //updateProduct.productId = 946;
+            //productRepo.UpdateProduct(updateProduct);
+            productRepo.DeleteProduct(946);
+            var productList = productRepo.GetAllProducts();
+            foreach(var product in productList)
+            {
+                Console.WriteLine(product.name);
+                Console.WriteLine(product.price);
+                Console.WriteLine(product.productId);
+                Console.WriteLine(product.categoryId);
+                Console.WriteLine(product.onSale);
+                Console.WriteLine(product.stockLevel);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            #endregion
         }
     }
 }
